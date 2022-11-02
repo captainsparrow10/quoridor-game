960aaf14c97e229a2e5b39e132c63059fce7731f
Root <114025437+root3920@users.noreply.github.com>
2022-11-02 17:17:44 -0500

    Update Peon.java

--- movilidad/Peon.java
+++ movilidad/Peon.java
@@ -1,4 +1,12 @@
 package movilidad;
 public class Peon {
+  int posicionX;
+  int posicionY;
   
+  
+  public peon(int posicionX, int posicionY){
+    this.posicionX = posicionX;
+    this.posicionY = posicionY;
+  }
+ 
 }
