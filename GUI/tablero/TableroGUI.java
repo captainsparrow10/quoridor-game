86cb933f90c7b77f5d0a3c9dacc623687009cc43
captainsparrow10 <javier1009rm@gmail.com>
2022-11-11 15:23:31 -0500

    GUI buttons part 1

--- GUI/tablero/TableroGUI.java
+++ GUI/tablero/TableroGUI.java
@@ -1,4 +1,5 @@
 package tablero;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Color;
@@ -7,57 +8,40 @@ import java.awt.Color;
 
 public class TableroGUI {
   public static void Tablero() {
-
-    JPanel peonPosition1 = new JPanel();
-    peonPosition1.setBounds(240, 0, 50, 50);
-    peonPosition1.setBackground(Color.red);
-
-    JPanel peonPosition2 = new JPanel();
-    peonPosition2.setBounds(240, 480, 50, 50);
-    peonPosition2.setBackground(Color.blue);
-
-    JFrame frame = new JFrame();
-    frame.setSize(600, 600);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    boolean isBlack = true;
-    boolean isPar = false;
-    frame.add(peonPosition1);
-    frame.add(peonPosition2);
-    for (int i = 0; i < 530;) {
-      if (isPar) {
-        for (int j = 0; j < 530; j += 50) {
-          JPanel whitePanel = new JPanel();
-          whitePanel.setBackground(Color.gray);
-          whitePanel.setBounds(j, i, 50, 10);
-          frame.add(whitePanel);
-        }
-        isPar = false;
-        isBlack = true;
-        i += 50;
-      } else {
-        for (int j = 0; j < 530;) {
-
-          if (isBlack) {
-            JPanel blackPanel = new JPanel();
-            blackPanel.setBackground(Color.black);
-            blackPanel.setBounds(j, i, 50, 50);
-            frame.add(blackPanel);
-            isBlack = false;
-            j += 50;
-          } else {
-            JPanel whitePanel = new JPanel();
-            whitePanel.setBackground(Color.gray);
-            whitePanel.setBounds(j, i, 10, 50);
-            frame.add(whitePanel);
-            isBlack = true;
-            j += 10;
-          }
-        }
-        isPar = true;
-        i += 10;
-      }
-    }
-
-    frame.setVisible(true);
+    JPanel panel = new JPanel();
+    // Fila 1
+    JButton f001 = new JButton();
+    JButton f002 = new JButton();
+    JButton f003 = new JButton();
+    JButton f004 = new JButton();
+    JButton f005 = new JButton();
+    JButton f006 = new JButton();
+    JButton f007 = new JButton();
+    JButton f008 = new JButton();
+    JButton f009 = new JButton();
+    JButton f010 = new JButton();
+    JButton f011 = new JButton();
+    JButton f012 = new JButton();
+    JButton f013 = new JButton();
+    JButton f014 = new JButton();
+    JButton f015 = new JButton();
+    JButton f016 = new JButton();
+    JButton f017 = new JButton();
+    // Fila 2
+    // Fila 3
+    // Fila 4
+    // Fila 5
+    // Fila 6
+    // Fila 7
+    // Fila 8
+    // Fila 9
+    // Fila 10
+    // Fila 11
+    // Fila 12
+    // Fila 13
+    // Fila 14
+    // Fila 15
+    // Fila 16
+    // Fila 17
   }
 }
